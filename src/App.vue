a0f391acb4fe1294fbf81ffc21063de45a725a0a
Anand Chowdhary <anandchowdhary@gmail.com>
2018-10-09 14:40:38 +0200

    Add code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div id="app">
     <div>Sample:</div>
-    <Camera />
+    <Camera ref="camera" width="300" height="300" :facingMode="facingMode" />
+    <button @click="clickPhoto">Click photo</button>
+    <button @click="toggleFacingMode">Toggle camera</button>
+    <div>Facing mode: {{facingMode}}</div>
+    <div><img alt="Your image" :src="image"></div>
   </div>
 </template>
 
 <script>
 import Camera from "./Camera.vue";
 export default {
+  data() {
+    return {
+      facingMode: "user",
+      image: ""
+    };
+  },
   components: {
     Camera
+  },
+  methods: {
+    clickPhoto() {
+      this.image = this.$refs.camera.click();
+    },
+    toggleFacingMode() {
+      this.facingMode = this.facingMode === "user" ? "environment" : "user";
+    }
   }
 };
 </script>
--- src/Camera.vue
+++ src/Camera.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>CAMERA {{image}}</div>
+  <div>
+    <div>CAMERA {{img}}</div>
+    <video ref="cameraVideo" />
+    <canvas id="cameraCanvas" ref="cameraCanvas" aria-hidden="true" />
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,69 @@ export default {
     return {
       img: "Hello"
     };
+  },
+  props: ["width", "height", "facingMode"],
+  mounted() {
+    this.updateFeed();
+  },
+  computed: {
+    everything() {
+      return this.width, this.height, this.facingMode;
+    }
+  },
+  watch: {
+    everything() {
+      this.updateFeed();
+    }
+  },
+  methods: {
+    updateFeed() {
+      const constraints = {
+        audio: false,
+        video: {
+          width: this.width || 1280,
+          height: this.height || 720,
+          facingMode: this.facingMode || "user"
+        }
+      };
+      this.$refs.cameraCanvas.style.width = (this.width || 1280) + "px";
+      this.$refs.cameraCanvas.style.height = (this.height || 720) + "px";
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(mediaStream => {
+          this.$refs.cameraVideo.srcObject = mediaStream;
+          this.$refs.cameraVideo.addEventListener("loadedmetadata", () => {
+            this.$refs.cameraVideo.play();
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    click() {
+      this.$refs.cameraCanvas
+        .getContext("2d")
+        .drawImage(
+          this.$refs.cameraVideo,
+          0,
+          0,
+          this.width || 1280,
+          this.height || 720
+        );
+      return this.$refs.cameraCanvas.toDataURL();
+    }
   }
 };
 </script>
+
+<style scoped>
+#cameraCanvas {
+  position: absolute !important;
+  clip: rect(1px, 1px, 1px, 1px);
+  padding: 0 !important;
+  border: 0 !important;
+  height: 1px !important;
+  width: 1px !important;
+  overflow: hidden;
+}
+</style>
