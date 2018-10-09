04401ccb727bb69a944dd8e4e1ff084b70bd3fec
Anand Chowdhary <anandchowdhary@gmail.com>
2018-10-09 14:06:06 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div>Sample:</div>
+    <Vuecam />
+  </div>
+</template>
+
+<script>
+import Vuecam from "./Vuecam.vue";
+export default {
+  components: {
+    Vuecam
+  }
+};
+</script>
---
+++ src/Vuecam.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>CAMERA {{image}}</div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      img: "Hello"
+    };
+  }
+};
+</script>
